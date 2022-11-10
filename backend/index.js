2a9c6d51a0494cb3ff3abdc3866800f2ba31485a
Vladimir_Leonov <2227331.spb@gmail.com>
2022-11-11 00:27:31 +0100

    get_first_img_from_dir

--- backend/index.js
+++ backend/index.js
@@ -11,32 +11,34 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({ origin: ['http://localhost:3000'] }));
 
+//получаем массив с картинками
 fs.readdir(`${__dirname}/uploads/`,(err, files) => {
     files.forEach(file => {
-      console.log(file);
-  if(file.length){
       fs.readdir(`${__dirname}/uploads/${file}`,(err, files) => {
-        files.forEach(file => {
-            console.log(file);
+            if(files){
+              files[0]
+           
+              app.use('/img', express.static(`${__dirname}/uploads/${file}`));
+            }
           })
         })
-      }
       })
-    })
+
  
 
-  //прочитать файл
-  fs.readFile(`./uploads/A011CA126_68f8f9e7-04ac-4395-998b-a50f99901a78/trace.json`, 'utf-8', (err, data) => {
+  // //прочитать файл
+  // fs.readFile(`./uploads/A011CA126_68f8f9e7-04ac-4395-998b-a50f99901a78/trace.json`, 'utf-8', (err, data) => {
   
-  })
+  // })
 
-  //получить имя дикторий в массиве
-  fs.readdir(`${__dirname}/uploads`, { withFileTypes: true }, (err, files) => {
-    files.filter(d => d.isDirectory()).map(d => d.name)
+  // //получить имя дикторий в массиве
+  // fs.readdir(`${__dirname}/uploads`, { withFileTypes: true }, (err, files) => {
+  //   files.filter(d => d.isDirectory()).map(d => d.name)
 
-    })
+  //   })
 
 //routes
 app.use('/upload', express.static('./uploads'));
+
 
 app.listen(PORT, () => console.log(`Server started on ${PORT} port`));
